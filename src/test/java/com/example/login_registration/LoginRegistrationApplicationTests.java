10b4387058d4d832996e2f96ee2296899d0b4c17
pbcuong781 <pbcuong7801@gmail.com>
2022-06-28 16:15:11 +0700

    Initial commit

---
+++ src/test/java/com/example/login_registration/LoginRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.login_registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
